463a7ab2a94fc945aa95511918b9f9fb099c9807
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-07-22 06:34:26 -0500

    Update and rename graph_metrics.py to metricas_grafos.py

--- collectors/metricas_grafos.py
+++ collectors/metricas_grafos.py
@@ -1,12 +1,17 @@
-#collectors/graph_metrics.py
+#collectors/metricas_grafos.py
 
 import networkx as nx
 
-def mean_node_distance(G: nx.Graph) -> float:
+def distancia_promedio_nodos(G: nx.Graph) -> float:
     """
     Calcula la distancia promedio entre todos los pares de nodos en un grafo.
     Si el grafo no está conectado, calcula la media sobre el componente gigante.
     """
+
+    #Grafo de 0 o 1 nodo
+    if G.number_of_nodes() < 2:
+        return 0  
+    
     if not nx.is_connected(G):
         # Tomar el componente conexo más grande
         G = G.subgraph(max(nx.connected_components(G), key=len)).copy()
