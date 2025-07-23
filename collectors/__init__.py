f8cbf7dcc5ecbd2260bc9571f2e1bd4ee1eaa158
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-07-23 11:31:21 -0500

    Update __init__.py

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -2,11 +2,12 @@
 
 from .descargador import crear_zip_url, cargar_grafo_desde_url
 from .extractor_metricas import extraer_estadisticas_red
-from .utiles import leer_archivo_aristas
+from .utiles import leer_archivo_aristas, guardar_resultados_en_duckdb
 
 __all__ = [
     "crear_zip_url",
     "cargar_grafo_desde_url",
     "extraer_estadisticas_red",
-    "leer_archivo_aristas"
+    "leer_archivo_aristas",
+    "guardar_resultados_en_duckdb"
 ]
