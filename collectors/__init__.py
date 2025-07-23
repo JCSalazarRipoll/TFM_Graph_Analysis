6d6a022d78061c6cae6c6b0e1b8bb5512cf66c56
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-07-23 07:10:14 -0500

    Update __init__.py

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,12 +1,12 @@
 #collectors.__init__.py
 
-from .downloader import build_zip_url, cargar_grafo_desde_url
-from .metrics_extractor import extraer_estadisticas_red
-from .utils import leer_archivo_edges
+from .descargador import crear_zip_url, cargar_grafo_desde_url
+from .extractor_metricas import extraer_estadisticas_red
+from .utiles import leer_archivos_aristas
 
 __all__ = [
-    "build_zip_url",
+    "crear_zip_url",
     "cargar_grafo_desde_url",
     "extraer_estadisticas_red",
-    "leer_archivo_edges"
+    "leer_archivos_aristas"
 ]
