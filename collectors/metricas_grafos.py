f143271dccb28165a88444fc02754b2a98866fe1
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-07-23 20:45:54 -0500

    Update metricas_grafos.py

--- collectors/metricas_grafos.py
+++ collectors/metricas_grafos.py
@@ -1,6 +1,11 @@
 #collectors/metricas_grafos.py
 
 import networkx as nx
+import logging
+
+# Configurar el logging (puedes mover esto a un archivo común si lo usas en varios lugares)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def distancia_promedio_nodos(G: nx.Graph) -> float:
     """
@@ -8,10 +13,9 @@ def distancia_promedio_nodos(G: nx.Graph) -> float:
     Si el grafo no está conectado, calcula la media sobre el componente gigante.
     """
 
-    #Grafo de 0 o 1 nodo
     if G.number_of_nodes() < 2:
         return 0  
-    
+
     if not nx.is_connected(G):
         # Tomar el componente conexo más grande
         G = G.subgraph(max(nx.connected_components(G), key=len)).copy()
@@ -19,5 +23,5 @@ def distancia_promedio_nodos(G: nx.Graph) -> float:
     try:
         return nx.average_shortest_path_length(G)
     except Exception as e:
-        print(f"Error al calcular la distancia promedio: {e}")
+        logger.warning(f"Error al calcular la distancia promedio: {e}")
         return -1
