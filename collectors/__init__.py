49074309a6e9f219b823eb39760eada53c5b2a13
JCSalazarRipoll <91150486+JCSalazarRipoll@users.noreply.github.com>
2025-07-31 06:51:10 -0500

    Update __init__.py

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,6 +1,6 @@
 #collectors.__init__.py
 
-from .descargador import crear_zip_url, cargar_grafo_desde_url
+from .descargador import crear_zip_url, cargar_grafo_desde_url, leer_config_desde_txt
 from .metricas_grafos import distancia_promedio_nodos
 from .utiles import leer_archivo_aristas, guardar_resultados_en_duckdb
 
@@ -8,6 +8,7 @@ from .utiles import leer_archivo_aristas, guardar_resultados_en_duckdb
 __all__ = [
     "crear_zip_url",
     "cargar_grafo_desde_url",
+    "leer_config_desde_txt",
     "distancia_promedio_nodos",
     "leer_archivo_aristas",
     "guardar_resultados_en_duckdb"
